037aabbe817d4a49e0516b2a39c4d6b8e55b85c5
Josh Taylor <joshotaylor@gmail.com>
2016-02-26 18:21:53 +0000

    Register the commands

--- src/MigrationsGeneratorServiceProvider.php
+++ src/MigrationsGeneratorServiceProvider.php
@@ -11,8 +11,16 @@ class MigrationsGeneratorServiceProvider extends ServiceProvider
      */
     public function register()
     {
+        // Register the Generator service provider
         if ($this->app->environment() == 'local') {
             $this->app->register('Laracasts\Generators\GeneratorsServiceProvider');
         }
+
+        // Register our commands
+        $this->app->singleton('command.josh.generate', function ($app) {
+            return $app['Josh\MigrationsGenerator\GenerateCommand'];
+        });
+
+        $this->commands('command.josh.generate');
     }
 }
