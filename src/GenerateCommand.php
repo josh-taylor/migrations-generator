43daf57169d5e919b0090fb51d37dde0ddef3c8c
Josh Taylor <josh@somarketing.com>
2016-02-26 16:20:58 +0000

    Initial commit

---
+++ src/GenerateCommand.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Josh\MigrationsGenerator;
+
+use Illuinate\Console\Command;
+
+class GenerateCommand extends Command
+{
+    /**
+     * The name and signature of the command
+     *
+     * @var string
+     */
+    protected $signature = 'migrate:generate';
+}
+
